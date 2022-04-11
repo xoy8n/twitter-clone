1a94df2591151d59bad97ea596e4217fc83ef817
xoy8n <s01057111917@gmail.com>
2022-04-11 16:11:35 +0900

    initial commit

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req,res)=>res.render("home"));
+app.get("/*", (req,res)=>res.redirect("/"))
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({server});
+
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser ✔");
+    socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+    socket.on("message", message => {
+        console.log(message.toString('utf8'));
+    });
+    socket.send("hello from Server!");
+});
+
+server.listen(3000, handleListen);
