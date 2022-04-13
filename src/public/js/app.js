da480e459d33889ea529f7be7aaedf1e0bb24550
“xoy8n” <s01057111917@gmail.com>
2022-04-13 12:42:51 +0900

    #1.7 Nicknames part one

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,6 +1,12 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 function handleOpen() {
   console.log("Connected to Server ✅");
@@ -9,18 +15,28 @@ function handleOpen() {
 socket.addEventListener("open", handleOpen);
 
 socket.addEventListener("message", (message) => {
-  console.log("New message: ", message.data);
+  const li = document.createElement("li"); 
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
   console.log("Disconnected from Server ❌");
 });
 
+//chat으로 보내는 메세지
 function handleSubmit(event) {
   event.preventDefault();
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value));
   input.value = "";
 }
 
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+}
+
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
